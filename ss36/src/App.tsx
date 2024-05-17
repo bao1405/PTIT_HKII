15c56096537ea5753d871ae5e864d28443add9be
bao1405 <147390596+bao1405@users.noreply.github.com>
2024-05-17 07:53:14 +0700

    update

---
+++ ss36/src/App.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import Bai1 from './components/bai1'
+import Bai2 from './components/bai2'
+import Bai3 from './components/bai3'
+import Bai4 from './components/bai4'
+import Bai5 from './components/bai5'
+import Bai6 from './components/bai6'
+import Bai7 from './components/bai7'
+import Bai8 from './components/bai8'
+import Bai9 from './components/bai9'
+import Bai10 from './components/bai10'
+export default function App() {
+  return (
+    <div>App
+      <Bai1></Bai1>
+      <Bai2></Bai2>
+      <Bai3></Bai3>
+      <Bai4></Bai4>
+      <Bai5></Bai5>
+      <Bai6></Bai6>
+      <Bai7></Bai7>
+      <Bai8></Bai8>
+      <Bai9></Bai9>
+      <Bai10></Bai10>
+    </div>
+  )
+}
